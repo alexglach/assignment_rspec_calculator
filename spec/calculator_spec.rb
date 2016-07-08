90169fc340c4d4ea698cc3eca416cfe156e658d9
Mike Lee <asackofwheat@gmail.com>
2016-07-08 13:03:23 -0400

    done with exercise

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -10,6 +10,10 @@ describe Calculator do
       expect(c).to be_a(Calculator)
     end
 
+    it 'should take an argument and not get an error' do
+      expect{Calculator.new(true)}.not_to raise_error
+    end
+
   end
 
   describe '#add' do
@@ -55,11 +59,11 @@ describe Calculator do
   describe '#divide' do
 
     it 'should divide two integers and return an integer if there is no remainder' do
-      expect(c.divide(4,2)).to be(2)
+      expect(c.divide(4,2)).to eq(2)
     end
 
     it 'should divide two integers and return a float if there is a remainder' do
-      expect(c.divide(3,2)).to be(1.5)
+      expect(c.divide(3,2)).to eq(1.5)
     end
 
     it 'should raise error if second argument is 0' do
@@ -71,15 +75,15 @@ describe Calculator do
   describe '#pow' do
   
     it 'should raise numbers to their power' do
-      expect(c.pow(3,3)).to be(27.0)
+      expect(c.pow(3,3)).to eq(27.0)
     end
 
     it 'should raise numbers to a power that is a float' do
-      expect(c.pow(27, 1/3.0)).to be(3.0)
+      expect(c.pow(27, 1/3.0)).to eq(3.0)
     end
 
     it 'should raise numbers to a power that is a negative number' do
-      expect(c.pow(10, -1)).to be(0.1)
+      expect(c.pow(10, -1)).to eq(0.1)
     end
 
   end  
@@ -95,18 +99,42 @@ describe Calculator do
     end
 
     it 'should return an integer for round roots' do
-      expect(c.sqrt(16)).to be(4)
+      expect(c.sqrt(16)).to eq(4)
     end
 
     it 'should return two-decimal digits for non-round roots' do
-      expect(c.sqrt(8)).to be(2.83)
+      expect(c.sqrt(8)).to eq(2.83)
     end
   end
 
   describe '#memory' do
   
     it 'memory should be nil if not given as an argument' do
-      
+      expect(c.memory).to eq(nil)
+    end
+
+    it 'memory can take a value' do
+      expect(c.memory=8).to eq(8)
+    end
+
+    it 'memory can store a value which can be read' do
+      c.memory=8
+      expect(c.memory).to eq(8)
+    end
+
+    it 'memory clears after being read' do
+      c.memory=8
+      c.memory
+      expect(c.memory).to eq(nil)
+    end
+
+  end
+
+  describe '#stringify' do
+
+    it 'turns outputs into strings' do
+      c2 = Calculator.new(true)
+      expect(c2.add(1,2)).to eq("3")
     end
 
   end
